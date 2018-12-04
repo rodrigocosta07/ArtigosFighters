eec50e5f07bbe47ccf1df0ca55e68d51378b7215
Rodrigo <rodrigo3mil@hotmail.com>
2018-12-03 23:23:38 -0300

    Correções na action de login

--- AppEnvioArtigos/AppEnvioArtigos/Controllers/ParticipantesController.cs
+++ AppEnvioArtigos/AppEnvioArtigos/Controllers/ParticipantesController.cs
@@ -66,7 +66,7 @@ namespace AppEnvioArtigos.Controllers
 
                         db.Participantes.Add(participante);
                         db.SaveChanges();
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Login", "Participantes");
 
                     }
 
